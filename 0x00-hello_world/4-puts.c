ef6d204d4bb13f22ebd670788e3782f6aea0d128
Mohamed-Deefallah <mohameddiaf021@gmail.com>
2023-06-15 17:21:38 -0700

    vvv

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
  * main - use to print "Programming is like building a multilingual puzzle
- * Return : 0 that we return to zero
+ * Return:0 if exited properly, non-zero otherwise
 */
 int main(void)
 {
