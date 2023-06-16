e567083911cf85f5493ae1fc267ff53ee2053e1d
Mohamed-Deefallah <mohameddiaf021@gmail.com>
2023-06-15 17:13:29 -0700

    vvv

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,8 @@
 #include <stdio.h>
+/**
+ * main use to print the required task
+ * after that we return to zero
+*/
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
