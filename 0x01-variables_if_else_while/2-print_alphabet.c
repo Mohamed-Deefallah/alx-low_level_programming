ff0d19659a2a00d451a0b811dc45eb53177c8095
Mohamed-Deefallah <mohameddiaf021@gmail.com>
2023-06-16 17:14:13 -0700

    vvv

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,18 +1,16 @@
 #include <stdio.h>
 /**
- * the mai function to print the alpha.in lowercase
- * th return must be in zero to success
+ * main - prints the size of various types
+ * Return: 0 if exited properly, non-zero otherwise
  */
 int main(void)
 {
 	char ch;
 
-	for (ch= 'a';ch<= 'z';ch++)
+	for (ch = 'a'; ch <= 'z'; ch++)
 	{
 		putchar(ch);
 	}
 	putchar('\n');
 	return (0);
 }
-	
-
