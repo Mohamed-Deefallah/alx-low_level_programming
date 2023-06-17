00ea00050ed4827f7c41b7213ef1fa43fac44388
Mohamed-Deefallah <mohameddiaf021@gmail.com>
2023-06-16 18:12:35 -0700

    vvv

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - prints the size of various types
+ * Return: 0 if exited properly, non-zero otherwise
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0 ; n < 10 ; n++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}
