6b1ca4963360281625bf6f99baead42db24c800c
Mohamed-Deefallah <mohameddiaf021@gmail.com>
2023-06-16 17:48:35 -0700

    vvv

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - prints the size of various types
+ * Return: 0 if exited properly, non-zero otherwise
+ */
+int main(void)
+{
+	char ch = 'a';
+
+	while (ch <= 'z')
+	{
+		if (ch != 'e' && ch != 'q')
+		{
+		puchar(ch);
+		}
+		ch++;
+	}
+	putchar('\n');
+	return (0);
+}
