28da74db54ab39135587baa8e0224a866785f8e7
Mohamed-Deefallah <mohameddiaf021@gmail.com>
2023-06-20 13:26:22 -0700

    vvv

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * main-print ten times lowercase alphabet
- * function print_alphabet_x10 -print ten times
+ * print_alphabet_x10 -print ten times
  * Return: sucess equal zero
  */
 
