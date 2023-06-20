31d60c6ad1dce2cae22f6ae9502c0488df444bd6
Mohamed-Deefallah <mohameddiaf021@gmail.com>
2023-06-20 13:59:31 -0700

    vvv

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * _islower - fuction to print lower case 
+ * @c: character to make checking
+ * Return: 1 if lowe cas and 0 if uppercase
+ */
+
+int _islower(int c)
+{
+	return (c >= 'a' && c <= 'z');
+}
