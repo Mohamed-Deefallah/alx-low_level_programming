3c4d77a34910d2e71e5cd48a57c3c1c4cfb347fa
Mohamed-Deefallah <mohameddiaf021@gmail.com>
2023-06-20 12:34:57 -0700

    vvv

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include <main.h>
+/**
+ * main -use to print th alphabet, in lowercase
+ * Return : sucsess when return zero
+ */
+
+void print_alphabet(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+
+	{
+		_putchar(c);
+		c++;
+	}
+	return (0);
+}
