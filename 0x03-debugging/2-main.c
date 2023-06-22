254bed41e6ddb6d358195ffbf72d36180d65d39d
Mohamed-Deefallah <mohameddiaf021@gmail.com>
2023-06-22 14:24:19 -0700

    vvv

--- 0x03-debugging/2-main.c
+++ 0x03-debugging/2-main.c
@@ -8,14 +8,14 @@
 
 int main(void)
 {
-        int a = 972;
+	int a = 972;
 	int b = -98;
-	int c = 0 ;
+	int c = 0;
 	int largest;
 
-        largest = largest_number(a, b, c);
+	largest = largest_number(a, b, c);
 
-        printf("%d is the largest number\n", largest);
+	printf("%d is the largest number\n", largest);
 
-        return (0);
+	return (0);
 }
