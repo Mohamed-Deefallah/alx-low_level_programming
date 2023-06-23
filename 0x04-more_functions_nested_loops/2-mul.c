700b61420cf6ee127c9a02ed6956004e20e1e4f2
Mohamed-Deefallah <mohameddiaf021@gmail.com>
2023-06-23 15:21:17 -0700

    vvv

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * mul - the multip function
+ * @a: the first number
+ * @b: the second number
+ * Return: the result of operation
+ */
+
+int mul(int a, int b)
+{
+	int result = a * b;
+
+	return (result);
+}
